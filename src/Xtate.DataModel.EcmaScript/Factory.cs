79c27b0a6e86cd020806c85b6d71118a4a7b3d27
Sergii Artemenko <xtadex@gmail.com>
2020-08-11 01:18:45 +0200

    Dynamic Factories implemented

--- src/Xtate.DataModel.EcmaScript/Factory.cs
+++ src/Xtate.DataModel.EcmaScript/Factory.cs
@@ -17,18 +17,16 @@
 
 #endregion
 
-using System.Diagnostics.CodeAnalysis;
 using Xtate.Annotations;
 
 namespace Xtate.DataModel.EcmaScript
 {
-	[PublicAPI]
-	[ExcludeFromCodeCoverage]
-	internal static class Res
+	[UsedImplicitly]
+	public class Factory : FactoryBase
 	{
-		public static string Format(string format, object? arg)                              => string.Format(Resources.Culture, format, arg);
-		public static string Format(string format, object? arg0, object? arg1)               => string.Format(Resources.Culture, format, arg0, arg1);
-		public static string Format(string format, object? arg0, object? arg1, object? arg2) => string.Format(Resources.Culture, format, arg0, arg1, arg2);
-		public static string Format(string format, params object?[] args)                    => string.Format(Resources.Culture, format, args);
+		public Factory()
+		{
+			Add(EcmaScriptDataModelHandler.Factory);
+		}
 	}
 }
